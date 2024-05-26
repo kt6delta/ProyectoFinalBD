5ce0ee739b54cd5547e364f19023e3bb3f910eb4
kt6delta <lcpreciadob@udistrital.edu.co>
2024-05-26 18:08:59 -0500

    requerimientos how component

---
+++ components/InputsRequerimiento.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import { ref, watch, defineEmits } from 'vue'
+
+let cod = ref(null)
+let salarioMax = ref("")
+let salarioMin = ref("")
+let nVacantes = ref("")
+let descfuncion = ref("")
+let descCarreras = ref("")
+let FechaReque = ref(null)
+
+const emit = defineEmits(['update:cod','update:salarioMax','update:salarioMin','update:nVacantes','update:descfuncion','update:descCarreras','update:FechaReque'])
+
+watch(cod, (newVal) => {
+  emit('update:cod', newVal)
+})
+
+watch(salarioMax, (newVal) => {
+  emit('update:salarioMax', newVal)
+})
+
+watch(salarioMin, (newVal) => {
+  emit('update:salarioMin', newVal)
+})
+
+watch(nVacantes, (newVal) => {
+  emit('update:nVacantes', newVal)
+})
+
+watch(descfuncion, (newVal) => {
+  emit('update:descfuncion', newVal)
+})
+
+watch(descCarreras, (newVal) => {
+  emit('update:descCarreras', newVal)
+})
+
+watch(FechaReque, (newVal) => {
+  emit('update:FechaReque', newVal)
+})
+
+let isDisable = ref(true)
+</script>
+
+<template>
+    <div class="grid md:grid-cols-2 gap-10 mx-auto max-w-4xl">
+        <!-- input General -->
+        <InputGroup>
+            <InputGroupAddon class="bg-black text-white">
+                <Icon name="material-symbols:person-outline" color="white" size="22" />
+            </InputGroupAddon>
+            <InputText type="number" :min="0" placeholder="ConsecReque" v-model="cod" :disabled="isDisable"/>
+        </InputGroup>
+
+        <InputGroup>
+            <InputGroupAddon class="bg-black text-white">
+                <Icon name="material-symbols:person-outline" color="white" size="22" />
+            </InputGroupAddon>
+            <InputText type="number" placeholder="SalarioMax" v-model="salarioMax" />
+        </InputGroup>
+
+        <InputGroup>
+            <InputGroupAddon class="bg-black text-white">
+                <Icon name="material-symbols:person-outline" color="white" size="22" />
+            </InputGroupAddon>
+            <InputText type="number" placeholder="SalarioMin" v-model="salarioMin" />
+        </InputGroup>
+
+        <InputGroup>
+            <InputGroupAddon class="bg-black text-white">
+                <Icon name="material-symbols:person-outline" color="white" size="22" />
+            </InputGroupAddon>
+            <InputText type="text" placeholder="DescFuncion" v-model="descfuncion"
+                @input="descfuncion = descfuncion.toLowerCase()" />
+        </InputGroup>
+
+        <InputGroup>
+            <InputGroupAddon class="bg-black text-white">
+                <Icon name="material-symbols:person-outline" color="white" size="22" />
+            </InputGroupAddon>
+            <InputText type="text" placeholder="DescCarreras" v-model="descCarreras"
+                @input="descCarreras = descCarreras.toLowerCase()" />
+        </InputGroup>
+
+        <InputGroup>
+            <InputGroupAddon class="bg-black text-white">
+                <Icon name="material-symbols:person-outline" color="white" size="22" />
+            </InputGroupAddon>
+            <InputText type="number" :min="1" placeholder="nVacantes" v-model="nVacantes" />
+        </InputGroup>
+
+        <div class="card flex flex-col md:flex-row gap-3">
+            <InputGroup>
+                <InputGroupAddon class="bg-black text-white">
+                    <Icon name="majesticons:calendar" color="white" size="22" />
+                </InputGroupAddon>
+                <FloatLabel>
+                    <label for="label-nacimiento">FechaCreacion </label>
+                    <Calendar dateFormat="dd/mm/yy" v-model="FechaReque" />
+                </FloatLabel>
+            </InputGroup>
+        </div>
+    </div>
+</template>
--- pages/requerimiento/[empleado].vue
+++ pages/requerimiento/[empleado].vue
@@ -2,25 +2,43 @@
 definePageMeta({
   layout: "landing",
 });
-
-import { useToast } from "primevue/usetoast";
-const toast = useToast();
-
-let cod = ref(null);
-let salarioMax = ref("");
-let salarioMin = ref("");
-let nVacantes= ref("");
-let descfuncion = ref("");
-let descCarreras = ref("");
-let FechaReque = ref(null);
-let consecReque = ref(null);
-
+import InputsRequerimiento from '@/components/InputsRequerimiento.vue';
 
 const save = () => {
   submit();
 };
 
 
+
+let cod = ref(null)
+let salarioMax = ref("")
+let salarioMin = ref("")
+let nVacantes = ref("")
+let descfuncion = ref("")
+let descCarreras = ref("")
+let FechaReque = ref(null)
+
+const updateCod = (newVal:any) => {
+  cod.value = newVal
+}
+const updateSalarioMax = (newVal:any) => {
+  salarioMax.value = newVal
+}
+const updateNVacantes = (newVal:any) => {
+  nVacantes.value = newVal
+}
+const updateDescfuncion = (newVal:any) => {
+  descfuncion.value = newVal
+}
+const updateDescCarreras = (newVal:any) => {
+  descCarreras.value = newVal
+}
+const updateFechaReque = (newVal:any) => {
+  FechaReque.value = newVal
+}
+const updateSalarioMin = (newVal:any) => {
+  salarioMin.value = newVal
+}
 
 let message = ref("");
 let create = ref(false);
@@ -30,7 +48,7 @@ let isIncomplete = ref(false);
 async function submit() {
   const CODEMPLEADO = localStorage.getItem('CODEMPLEADO');
   try {
-    if ( !salarioMax.value || !salarioMin.value || !FechaReque.value || !descfuncion.value || !descCarreras.value || !nVacantes.value) {
+    if (!salarioMax.value || !salarioMin.value || !FechaReque.value || !descfuncion.value || !descCarreras.value || !nVacantes.value) {
       message.value = "Faltan datos";
       isIncomplete.value = true;
       return;
@@ -43,101 +61,44 @@ async function submit() {
         "SALARIOMAX": salarioMax.value,
         "SALARIOMIN": salarioMin.value,
         "DESCFUNCION": descfuncion.value,
-        "DESCCARRERAS":descCarreras.value,
+        "DESCCARRERAS": descCarreras.value,
         "NVACANTES": nVacantes.value
       }
     })
     message.value = "requerimiento creado";
     create.value = true;
-  } 
-  
+  }
+
   catch (error) {
     console.error('Error during fetch:', error);
     message.value = "Ya existe un requerimiento";
     alredyCreate.value = true;
   }
 }
-
+const currentDate = new Date().toLocaleDateString();
 </script>
 
 <template>
-    <Card class="mt-[5vh]" style="overflow: hidden">
-      <template #header>
-        <LandingSectionhead>
-          <template v-slot:title>Crear Requerimiento</template>
-          <template v-slot:desc>We find the best people.<br> fecha:</template>
-        </LandingSectionhead>
-      </template>
-      <template #content>
-        <div class="grid md:grid-cols-2 gap-10 mx-auto max-w-4xl mt-16">
-          <!-- input General -->
-          <InputGroup>
-          <InputGroupAddon class="bg-black text-white">
-            <Icon name="material-symbols:person-outline" color="white" size="22" />
-          </InputGroupAddon>
-          <InputText type="number" :min="0" placeholder="ConsecReque" v-model="cod" />
-        </InputGroup>
-
-          <InputGroup>
-          <InputGroupAddon class="bg-black text-white">
-            <Icon name="material-symbols:person-outline" color="white" size="22" />
-          </InputGroupAddon>
-          <InputText type="number" placeholder="SalarioMax" v-model="salarioMax" />
-        </InputGroup>
-
-        <InputGroup>
-          <InputGroupAddon class="bg-black text-white">
-            <Icon name="material-symbols:person-outline" color="white" size="22" />
-          </InputGroupAddon>
-          <InputText type="number" placeholder="SalarioMin" v-model="salarioMin"/>
-        </InputGroup>
-
-        <InputGroup>
-          <InputGroupAddon class="bg-black text-white">
-            <Icon name="material-symbols:person-outline" color="white" size="22" />
-          </InputGroupAddon>
-          <InputText type="text" placeholder="DescFuncion" v-model="descfuncion" @input="descfuncion = descfuncion.toLowerCase()" />
-        </InputGroup>
-
-        <InputGroup>
-          <InputGroupAddon class="bg-black text-white">
-            <Icon name="material-symbols:person-outline" color="white" size="22" />
-          </InputGroupAddon>
-          <InputText type="text" placeholder="DescCarreras" v-model="descCarreras" @input="descCarreras = descCarreras.toLowerCase()" />
-        </InputGroup>
-
-        <InputGroup>
-          <InputGroupAddon class="bg-black text-white">
-            <Icon name="material-symbols:person-outline" color="white" size="22" />
-          </InputGroupAddon>
-          <InputText type="number" :min="1" placeholder="nVacantes" v-model ="nVacantes"/>
-        </InputGroup>
-
-        <div class="card flex flex-col md:flex-row gap-3">
-          <InputGroup>
-            <InputGroupAddon class="bg-black text-white">
-              <Icon name="majesticons:calendar" color="white" size="22" />
-            </InputGroupAddon>
-            <FloatLabel>
-              <label for="label-nacimiento">FechaCreacion </label>
-              <Calendar dateFormat="dd/mm/yy" v-model="FechaReque" />
-            </FloatLabel>
-          </InputGroup>
-        </div>
-
-
-        </div>
-      </template>
-      <template #footer>
-        <div class="flex justify-center align-middle gap-3 mt-1">
-          <Button label="Save" @click="save" rounded severity="contrast" icon="pi pi-check" iconPos="right"
-            class="text-white bg-black px-2 py-2"></Button>
-        </div>
-      </template>
-    </Card>
-    <div class="card flex flex-wrap items-center justify-center gap-3 mt-16">
+  <Card class="mt-[5vh]" style="overflow: hidden">
+    <template #header>
+      <LandingSectionhead>
+        <template v-slot:title>Crear Requerimiento</template>
+        <template v-slot:desc>We find the best people.<br> fecha: {{ currentDate }}</template>
+      </LandingSectionhead>
+    </template>
+    <template #content>
+      <InputsRequerimiento  @update:cod="updateCod" @update:salarioMax="updateSalarioMax" @update:salarioMin="updateSalarioMin" @update:nVacantes="updateNVacantes" @update:descfuncion="updateDescfuncion" @update:descCarreras="updateDescCarreras" @update:FechaReque="updateFechaReque"/>
+    </template>
+    <template #footer>
+      <div class="flex justify-center align-middle gap-3 mt-1">
+        <Button label="Save" @click="save" rounded severity="contrast" icon="pi pi-check" iconPos="right"
+          class="text-white bg-black px-2 py-2"></Button>
+      </div>
+    </template>
+  </Card>
+  <div class="card flex flex-wrap items-center justify-center gap-3 mt-16">
     <InlineMessage v-if="isIncomplete" severity="error">{{ message }}</InlineMessage>
     <InlineMessage v-else-if="alredyCreate" severity="warn">{{ message }}</InlineMessage>
     <InlineMessage v-else-if="create" severity="success">{{ message }}</InlineMessage>
-    </div>
+  </div>
 </template>
