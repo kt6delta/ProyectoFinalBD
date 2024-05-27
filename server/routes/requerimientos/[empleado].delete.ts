bddcb271c4a4cd1263fbfcc4cf8813fc67d7c3d8
kt6delta <lcpreciadob@udistrital.edu.co>
2024-05-27 06:31:05 -0500

    .

--- server/routes/requerimientos/[empleado].delete.ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler((event) => {
-    const name = getRouterParam(event, 'name')
-  
-    return{
-      hello: `Hello, ${name}! ${process.env.PASSWORD}`
-    } 
-    
-  })
